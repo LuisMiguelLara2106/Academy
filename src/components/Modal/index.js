e016067afc086cf3eff9789547a76ab0bcc62de8
miguellara2106 <miguel.lara@decimetrix.com>
2021-11-26 20:57:01 -0500

    improvements were added to the modal module

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useRef, useEffect, useCallback} from 'react';
+import ReactDOM from "react-dom";
+import {useSpring, animated} from 'react-spring';
 
 import modalImg from '../../images/outer_space.svg';
 
@@ -9,14 +11,49 @@ import {
      ModalWrapper,
      ModalImg,
      ModalText,
-     CloseModalButton
+     CloseModalButton,
     } from './ModalElements';
 
 const Modal = ({ showModal, setShowModal }) => {
+
+    const modalRef = useRef()
+
+    const animation = useSpring({
+        config: {
+            duration: 350 
+        },
+        opacity: showModal ? 1 : 0,
+        transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+    });
+
+    const closeModal = e => {
+        if (modalRef.current === e.target) {
+            setShowModal(false);
+        }
+    };
+
+    const keyPress = useCallback(
+        e => {
+            if (e.key === 'Escape' && showModal) {
+                setShowModal(false);
+            }
+        },
+        [setShowModal, showModal]
+    );
+
+    useEffect(
+        () => {
+            document.addEventListener('keydown', keyPress);
+            return () => document.removeEventListener('keydown', keyPress);
+        },
+        [keyPress]
+    );
+
     return (
         <>
           {showModal ? (
-            <Background>
+            <Background ref={modalRef} onClick={closeModal}>
+                <animated.div style={animation}>
                 <ModalWrapper showModal={showModal}>
                     <ModalImg src={modalImg}/>
                     <ModalContent>
@@ -29,6 +66,7 @@ const Modal = ({ showModal, setShowModal }) => {
                         onClick={() => setShowModal(prev => !prev)}
                     />
                 </ModalWrapper>
+                </animated.div>
             </Background>
         ) : null}
         </>
